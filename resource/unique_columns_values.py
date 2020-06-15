8c84c9c92684181db6c3d91271a455121c90b424
admin <admin>
2020-06-15 18:23:57 -0400

    Edited files

--- resource/unique_columns_values.py
+++ resource/unique_columns_values.py
@@ -1,7 +1,7 @@
 def do(payload, config, plugin_config, inputs):
     
     print("Here!")
-    print(payload)
+    print(config)
     
     choices = [
         { "value" : "val1", "label" : "Value 1"},
