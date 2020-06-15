d170a4bd7db52dd170584b8bab40265c75241e48
admin <admin>
2020-06-15 18:16:07 -0400

    Edited files

--- custom-recipes/scatter-plot-3d/unique_columns_values.py
+++ custom-recipes/scatter-plot-3d/unique_columns_values.py
@@ -1,6 +1,8 @@
 def do(payload, config, plugin_config, inputs):
-  choices = [
-    { "value" : "val1", "label" : "Value 1"},
-    { "value" : "val2", "label" : "Value 2"}
-  ]
+    
+    choices = [
+        { "value" : "val1", "label" : "Value 1"},
+        { "value" : "val2", "label" : "Value 2"}
+    ]
+    
 return {"choices": choices}
