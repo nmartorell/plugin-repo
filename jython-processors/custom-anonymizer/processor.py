0dadd1d022161f275a8a51620b23dc559842eaf1
admin <admin>
2020-06-16 13:39:07 -0400

    Edited files

--- jython-processors/custom-anonymizer/processor.py
+++ jython-processors/custom-anonymizer/processor.py
@@ -6,7 +6,7 @@ def process(row):
     input_column = params["input_column"]
 
     # Retrieve the list of words to anonymize (defined as a plugin parameter)
-    w = get_plugin_config()
+    #w = get_plugin_config()
     
     #print("here!")
     #print(w)
