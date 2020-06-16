1343a0395a2b98b35dfbf8f197a80c4758274ac1
admin <admin>
2020-06-16 14:22:32 -0400

    Edited files

--- jython-processors/custom-anonymizer/processor.py
+++ jython-processors/custom-anonymizer/processor.py
@@ -1,13 +1,16 @@
+# Note: this processor anonymizes in a caseless manner.
+
 def process(row):
     
-    # List of words to anonymize (assumes all lowercase)
+    # List of words to anonymize (ensure caseless)
     words_to_anonymize = ["tags", "doll", "marker", "heart", "colour"] 
+    words_to_anonymize = [w.casefold() for w in words_to_anonymize]
     
     # Retrieve the user-defined input column
     text_column = params["input_column"]
 
     # anonymize words from list
-    text_list = row[text_column].lower().split(" ")
-    text_list_anon = [w if w not in words_to_anonymize else "****" for w in text_list]
+    text_list = row[text_column].split(" ")
+    text_list_anon = [w if w.casefold() not in words_to_anonymize else "****" for w in text_list]
     
     return " ".join(text_list_anon)
