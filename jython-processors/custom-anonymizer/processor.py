9243ed1516f6c897d9c935037edd72bc7e9cab90
admin <admin>
2020-06-16 11:05:14 -0400

    Edited files

--- jython-processors/custom-anonymizer/processor.py
+++ jython-processors/custom-anonymizer/processor.py
@@ -1,6 +1,13 @@
 # This file is the actual code for the custom Jython step custom-anonymizer
 
 def process(row):
+    
+    # Retrieve the user-defined input column
     input_column = params["input_column"]
 
+    # Retrieve the list of words to anonymize (defined as a plugin parameter)
+    words_to_anonymize = 
+    
+    # dataiku.customrecipe.get_plugin_config()
+    
     return input_column
