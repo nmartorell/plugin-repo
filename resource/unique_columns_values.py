2c78f1b2056a7ec81a3310a1f84e77345b242243
admin <admin>
2020-06-16 09:30:59 -0400

    Edited files

--- resource/unique_columns_values.py
+++ resource/unique_columns_values.py
@@ -5,19 +5,13 @@ def do(payload, config, plugin_config, inputs):
     # Retrieve filter column
     filter_column = config["filter_column"]
     
-    # Retrieve input dataset
-    input_dataset = inputs[0]["fullName"]
+    # Retrieve input dataset as dataframe
+    input_dataset_name = inputs[0]["fullName"]
     
-    dataset = dataiku.Dataset(input_dataset)
+    dataset = dataiku.Dataset(input_dataset_name)
     df = dataset.get_dataframe()
     
-    unique_vals = df[filter_column].unique().tolist()
-    
-    print(unique_vals)
-    
-    choices = [
-        { "value" : "val1"},
-        { "value" : "val2"}
-    ]
+    # Generate list of dictionaries with unique values in column
+    choices = [{"value" : x, "label" : x} for x in df[filter_column].unique().tolist()]
     
     return {"choices": choices}
