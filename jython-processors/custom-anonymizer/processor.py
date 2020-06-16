146eea02d8ec8fd1ef1a1ec354884079affb0abe
admin <admin>
2020-06-16 10:49:33 -0400

    Edited files

--- jython-processors/custom-anonymizer/processor.py
+++ jython-processors/custom-anonymizer/processor.py
@@ -1,11 +1,6 @@
 # This file is the actual code for the custom Jython step custom-anonymizer
 
-# global- and project-level variables are passed as a dss_variables dict
-
-# the step parameters are passed as a params dict
-
-# Define here a function that returns the result of the step.
 def process(row):
-    # row is a dict of the row on which the step is applied
+    input_column = params["input_column"]
 
     return len(row)
