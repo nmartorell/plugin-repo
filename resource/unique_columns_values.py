ecacd95f68829332a9d777a61e97f23b742971b6
admin <admin>
2020-06-16 09:15:24 -0400

    Edited files

--- resource/unique_columns_values.py
+++ resource/unique_columns_values.py
@@ -1,7 +1,14 @@
 def do(payload, config, plugin_config, inputs):
     
-    print("Here!")
-    print(config)
+    print("here!")
+    print(inputs)
+    
+    # Retrieve filter column
+    filter_column = config["filter_column"]
+    
+    # Create list of unique values in filter column
+    
+    
     
     choices = [
         { "value" : "val1", "label" : "Value 1"},
