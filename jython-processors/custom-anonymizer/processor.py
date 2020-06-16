282adf842f3d89965acbd1a7eff1e9821519cb84
admin <admin>
2020-06-16 14:11:42 -0400

    Edited files

--- jython-processors/custom-anonymizer/processor.py
+++ jython-processors/custom-anonymizer/processor.py
@@ -7,7 +7,7 @@ def process(row):
     text_column = params["input_column"]
 
     # anonymize words from list
-    text_list = " ".split(row[text_column].lower())
+    text_list = row[text_column].lower().split(" ")
     text_list_anon = [w if w not in words_to_anonymize else "****" for w in text_list]
     
     #return " ".join(text_list_anon)
